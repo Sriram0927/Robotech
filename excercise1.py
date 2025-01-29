ddec616f7818d7f79151b01677539d41800a2048
Sriram0927 <144515369+Sriram0927@users.noreply.github.com>
2025-01-29 08:08:15 +0530

    Update excercise1.py

--- excercise1.py
+++ excercise1.py
@@ -1,4 +1,4 @@
-# Write your code below this line
+# This is a simple program using print statement
 print("Mix 500g of Flour,10g Yeast and 300ml Water in a bowl.")
 print("Knead the dough for 10 minutes.")
 print("Add 3g of Salt.")
